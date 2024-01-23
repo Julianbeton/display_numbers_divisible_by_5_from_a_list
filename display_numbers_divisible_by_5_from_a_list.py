fe249d999417336b80844d131fa0ce13f75d94bf
Julianbeton <julianibeton@iskolarngbayan.pup.edu.ph>
2024-01-23 23:18:14 +0800

    uploading final code

--- display_numbers_divisible_by_5_from_a_list.py
+++ display_numbers_divisible_by_5_from_a_list.py
@@ -10,6 +10,8 @@
 # 55
 
 given_list = [10, 20, 33, 46, 55]
+print("\033[1;32;40mThe given list is: ", given_list)
+print("Numbers that are divisible by 5 is:")
 
 for number in given_list:
     if number % 5 == 0:
